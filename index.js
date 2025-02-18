883a8e09caf1944733951306a8750f09a9f913ce
Mr-Courteous <inumiduncourteous@gmail.com>
2025-02-18 10:58:48 +0100

    another important change

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const connectDB = require('./connection');
 const cors = require('cors');
-const getRoute = require ('./Routes/getRoutes');
-const postRoute = require ('./Routes/postRoutes');
-const putRoute = require ('./Routes/putRoutes');
+const bodyParser = require ('body-parser')
+const getRoute = require('./Routes/getRoutes');
+const postRoute = require('./Routes/postRoutes');
+const putRoute = require('./Routes/putRoutes');
 const deleteRoute = require('./Routes/deleteRoutes');
-const registerAndForgetPasswordRoutes = require ('./Routes/teacherRegRoutes');
-const loginRoute = require ('./Routes/loginRoutes')
-const forgotPasswordRoutes = require ('./Routes/forgotPasswordroutes')
-const dashboard = require ('./Routes/dashboard')
+const registerAndForgetPasswordRoutes = require('./Routes/teacherRegRoutes');
+const loginRoute = require('./Routes/loginRoutes')
+const forgotPasswordRoutes = require('./Routes/forgotPasswordroutes')
+const dashboard = require('./Routes/dashboard')
 
 
 
@@ -18,19 +19,25 @@ const app = express()
 
 connectDB()
 
-
-app.use(express.json()); -
-
-  app.use(cors()); 
+  
 
 
-app.use(getRoute); 
+
+app.use(express.json());
+
+app.use(cors());
+
+
+
+app.use(bodyParser.json());
+
+app.use(getRoute);
 app.use(postRoute);
 app.use(putRoute);
 app.use(registerAndForgetPasswordRoutes)
 app.use(forgotPasswordRoutes)
 app.use(loginRoute);
-app.use (dashboard);
+app.use(dashboard);
 // app.use(deleteRoute);
 
 
