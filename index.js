151a137463f80ec260b16af2f5223be62853102b
Mr-Courteous <inumiduncourteous@gmail.com>
2025-02-18 09:38:42 +0100

    still adding up stuffs for editing

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const express = require('express');
-const app = express();
-const port = 3000; // You can choose any port
+const express = require('express')
+const connectDB = require('./connection');
+const cors = require('cors');
+const getRoute = require ('./Routes/getRoutes');
+const postRoute = require ('./Routes/postRoutes');
+const putRoute = require ('./Routes/putRoutes');
+const deleteRoute = require('./Routes/deleteRoutes');
+const registerAndForgetPasswordRoutes = require ('./Routes/teacherRegRoutes');
+const loginRoute = require ('./Routes/loginRoutes')
+const forgotPasswordRoutes = require ('./Routes/forgotPasswordroutes')
+const dashboard = require ('./Routes/dashboard')
+
+
+
+
+
+const app = express()
+
+connectDB()
+
+
+app.use(express.json()); -
+
+  app.use(cors()); 
+
+
+app.use(getRoute); 
+app.use(postRoute);
+app.use(putRoute);
+app.use(registerAndForgetPasswordRoutes)
+app.use(forgotPasswordRoutes)
+app.use(loginRoute);
+app.use (dashboard);
+// app.use(deleteRoute);
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Broadsheet Application says Hello !');
 });
 
+
+
+
+
+
+
+
+const port = 5000
+
+
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})  
